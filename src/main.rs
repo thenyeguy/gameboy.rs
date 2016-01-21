0289ea2d08ec9715405896640f4c60beb5b71b7a
Michael Nye <thenyeguy@gmail.com>
2016-01-20 20:28:22 -0800

    Read in ROM

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate docopt;
+extern crate rustc_serialize;
+
+use std::fs::File;
+use std::io::{self, Read};
+use std::path::Path;
+
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_rom: String,
+}
+
+const USAGE: &'static str = "
+Usage: gamebody <rom>
+       gamebody (-h | --help)
+
+Options:
+  -h --help     Show this screen.
+";
+
+fn load_rom<P: AsRef<Path>>(path: P) -> io::Result<Vec<u8>> {
+    let mut file = try!(File::open(path));
+    let mut buffer = Vec::new();
+    try!(file.read_to_end(&mut buffer));
+    Ok(buffer)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = docopt::Docopt::new(USAGE)
+                                    .and_then(|d| d.decode())
+                                    .unwrap_or_else(|e| e.exit());
+
+    println!("Loading ROM: {}", args.arg_rom);
+    let rom = load_rom(args.arg_rom).expect("Failed to load ROM");
 }
